6dfba306e95f98c07664d4182bba16709b905653
Jamie McLaughlin <jamiem@beachbody.com>
2023-04-14 15:41:35 -0700

    create files

---
+++ typescript/makefiles.sh
@@ -0,0 +1,31 @@
+mkdir -p src/controllers
+mkdir -p src/handlers
+mkdir -p src/services
+mkdir -p src/repositories
+mkdir -p src/entities
+mkdir -p src/utils
+mkdir -p src/__tests__
+
+touch src/controllers/favorite.controller.ts
+touch src/handlers/favorite.handler.ts
+touch src/services/favorite.service.ts
+touch src/repositories/favorite.repository.ts
+touch src/entities/favorite.ts
+touch src/utils/appError.ts
+touch src/utils/extractRequest.ts
+touch src/utils/request.ts
+touch src/utils/statusCode.ts
+touch src/utils/validation.ts
+touch src/index.ts
+
+touch src/__tests__/favorite.controller.test.ts
+touch src/__tests__/favorite.repository.test.ts
+touch src/__tests__/favorite.service.test.ts
+
+touch README.md
+touch Dockerfile
+touch serverless.yml
+touch swagger.yaml
+touch jest.config.js
+touch tsconfig.json
+touch package.json

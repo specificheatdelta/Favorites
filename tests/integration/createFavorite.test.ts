c0234ef261cb2206a2bbf2483a611e4d6e0a046d
fahadahsan14 <34250130+fahadahsan14@users.noreply.github.com>
2023-05-10 09:49:20 -0700

    Added TCs for create. New updateFavorite.ts file

--- tests/integration/createFavorite.test.ts
+++ tests/integration/createFavorite.test.ts
@@ -10,5 +10,24 @@ describe('Post Favorite', () => {
         })
         expect(response.status).toBe(201)}
         )
-    }
+    test('should return 400 and not create a new favorite', async () => {  
+        const response = await axios.post('http://localhost:3000/favorites', { 
+            'profileId': 'a0bvbd99-5f01g-5ja7-cc2d-2da1bd380a11',   
+            'title': 'Test Favorite',
+            'category': ['Workout', 'Meditation'],
+            'contentId': 'Alpha Omigron Pi',
+        })
+        expect(response.status).toBe(400)}
+        )
+    // test Deleting a Favorite
+        test('should return 204 and delete a favorite', async () => {
+            const response = await axios.delete('http://localhost:3000/favorites/235')
+            expect(response.status).toBe(204)}
+            )
+    //test updating a favorite
+        test('should return 200 and update a favorite', async () => {
+            
+
+
+        }
     )
